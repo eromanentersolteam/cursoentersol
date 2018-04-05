8ae36d3a037fb8fa68f5b3a038eaeba15b91973a
Edgar Roman <eroman@entersolteam.com.mx>
2018-04-05 18:05:10 -0500

    Agregar componente navbar

--- Dia4/src/app/chart-pie/chart-pie.component.ts
+++ Dia4/src/app/chart-pie/chart-pie.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { PoblacionMexicoService } from '../services/poblacion-mexico.service';
 
 @Component({
   selector: 'app-chart-pie',
   templateUrl: './chart-pie.component.html',
   styleUrls: ['./chart-pie.component.css']
 })
-export class ChartPieComponent implements OnInit {
+export class ChartPieComponent {
 
-  constructor() { }
+  public pieChartLabels:string[] = ['Mujeres', 'Hombres'];
+  public pieChartData:number[] = [0, 0];
+  public pieChartType:string = 'pie';
+  public lineChartLabels:Array<any> = [];
+  public comparacion:string;
 
-  ngOnInit() {
+  constructor(private poblacionService:PoblacionMexicoService) {
+    for(let i = 2012; i <= 2018; i++) {
+      this.lineChartLabels.push(i);
+    }
+
+    let pieChartData:number[] = [0,0];
+    this.lineChartLabels.forEach((value,index) => {
+      this.poblacionService.getPoblacionMexicoPorAño(value).subscribe(res => {
+        pieChartData[0] += res['females'];
+        pieChartData[1] += res['males'];
+        this.pieChartData =  pieChartData;
+        //console.log(this.pieChartData);
+      });
+    });
+
+    //this.comparacion = (this.pieChartData[0] < this.pieChartData[1]) ? 'Ganan los Hombres' : 'Ganan las Mujeres';
+
+  }
+
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+ 
+  public chartHovered(e:any):void {
+    console.log(e);
   }
 
 }
